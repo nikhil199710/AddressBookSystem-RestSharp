c1deb0b6870791ab805fd9de0292ce748a47692e
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-11-17 04:15:32 +0530

    DeleteDataFromServer

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -20,33 +20,18 @@ namespace MSTestForRestApiOperations
         }
 
         /// <summary>
-        /// UC23
-        /// Tests the add multiple entries using post operation.
+        /// UC25
+        /// Tests the delete data using delete operation.
         /// </summary>
         [TestMethod]
-        public void TestAddMultipleEntriesUsingPostOperation()
+        public void TestDeleteDataUsingDeleteOperation()
         {
-            //adding multiple employees to table
-            List<ContactInfo> contactList = new List<ContactInfo>();
-            contactList.Add(new ContactInfo { name = "nik", address = "pat", phoneNumber = "3124211", email ="nik.com", contactType = "Family" });
-            contactList.Add(new ContactInfo { name = "aky", address = "kol", phoneNumber = "211331313", email = "ak.com", contactType = "Frnd" });
-            foreach (ContactInfo contact in contactList)
-            {
-                RestRequest request = new RestRequest("/contacts", Method.POST);
-                JObject jObject = new JObject();
-                jObject.Add("name", contact.name);
-                jObject.Add("address", contact.address);
-                jObject.Add("phoneNumber", contact.phoneNumber);
-                jObject.Add("email", contact.email);
-                jObject.Add("contactType", contact.contactType);
-                request.AddParameter("application/json", jObject, ParameterType.RequestBody);
-                IRestResponse response = client.Execute(request);
-                //Assert
-                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
-                //derserializing object for assert and checking test case
-                ContactInfo dataResponse = JsonConvert.DeserializeObject<ContactInfo>(response.Content);
-                Assert.AreEqual(contact.name, dataResponse.name);
-            }
+            //Arrange
+            RestRequest request = new RestRequest("contacts/11", Method.DELETE);
+            //Act
+            IRestResponse response = client.Execute(request);
+            //Assert
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
         }
     }
 }
