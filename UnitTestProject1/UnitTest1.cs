5a51850bfbe4e9a06a225ee6de248ec3dd68b628
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-11-17 04:07:45 +0530

    AddMultipleData

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -20,17 +20,33 @@ namespace MSTestForRestApiOperations
         }
 
         /// <summary>
-        /// UC1
-        /// Tests the retrieving data function using get operation.
+        /// UC23
+        /// Tests the add multiple entries using post operation.
         /// </summary>
         [TestMethod]
-        public void TestRetrievingDataUsingGetOperation()
+        public void TestAddMultipleEntriesUsingPostOperation()
         {
-            RestRequest request = new RestRequest("/contacts", Method.GET);
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
-            List<ContactInfo> dataResponse = JsonConvert.DeserializeObject<List<ContactInfo>>(response.Content);
-            Assert.AreEqual(3, dataResponse.Count);
+            //adding multiple employees to table
+            List<ContactInfo> contactList = new List<ContactInfo>();
+            contactList.Add(new ContactInfo { name = "nik", address = "pat", phoneNumber = "3124211", email ="nik.com", contactType = "Family" });
+            contactList.Add(new ContactInfo { name = "aky", address = "kol", phoneNumber = "211331313", email = "ak.com", contactType = "Frnd" });
+            foreach (ContactInfo contact in contactList)
+            {
+                RestRequest request = new RestRequest("/contacts", Method.POST);
+                JObject jObject = new JObject();
+                jObject.Add("name", contact.name);
+                jObject.Add("address", contact.address);
+                jObject.Add("phoneNumber", contact.phoneNumber);
+                jObject.Add("email", contact.email);
+                jObject.Add("contactType", contact.contactType);
+                request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                //Assert
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                //derserializing object for assert and checking test case
+                ContactInfo dataResponse = JsonConvert.DeserializeObject<ContactInfo>(response.Content);
+                Assert.AreEqual(contact.name, dataResponse.name);
+            }
         }
     }
 }
